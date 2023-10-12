f7fda71d0437e585b87e797afc964e7f699a546d
Lukman10a <abdulrauflukman9@gmail.com>
2023-10-12 02:48:11 +0100

    create project and projects component

--- components/sections/Project.tsx
+++ components/sections/Project.tsx
@@ -34,13 +34,25 @@ export default function Project({
       <section className="bg-gray-200max-w-[42rem] border border-black/5 rounded-lg overflow-hidden relative sm:h-[20rem] hover:bg-gray-100 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:b-white/20 ">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p>{description}</p>
-          <ul>
+          <p className="mt-2 leading-relaxed text-gray-600 dark:text-white/70">
+            {description}
+          </p>
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
-              <li key={index}>{tag}</li>
+              <li
+                key={index}
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+              >
+                {tag}
+              </li>
             ))}
           </ul>
-          <Image src={url} alt="my live projects" quality={95} />
+          <Image
+            src={url}
+            alt="my live projects"
+            quality={95}
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
+          />
         </div>
       </section>
     </motion.div>
